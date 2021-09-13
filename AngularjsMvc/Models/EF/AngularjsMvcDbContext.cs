e646f236572e39e06cd0c50cf2525e7566135616
marcoscrocci <marcoscrocci@gmail.com>
2021-09-12 23:02:02 -0300

    First commit

---
+++ AngularjsMvc/Models/EF/AngularjsMvcDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace AngularjsMvc.Models.EF
+{
+    public class AngularjsMvcDbContext : DbContext
+    {
+        public AngularjsMvcDbContext(): base("name = AngularjsMvcDbContext")
+        {
+
+        }
+
+        // Setting EF Convention
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Use singular table name
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
